aa591c2e0a6e1f37fe4a292483cc44b2e74aafda
hector <hx.euwx@gmail.com>
2024-04-27 18:04:21 -0400

    Ajout de la bottomnavbar sur les activity_creer_plan_voyage.xml activity_creer_voyage.xml

--- app/src/main/java/uqac/dim/travelmanager/CreerPlanVoyageActivity.java
+++ app/src/main/java/uqac/dim/travelmanager/CreerPlanVoyageActivity.java
@@ -15,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import uqac.dim.travelmanager.models.Voyage;
 
 public class CreerPlanVoyageActivity extends AppCompatActivity {
@@ -76,24 +78,50 @@ public class CreerPlanVoyageActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton imageButtonAccueil = findViewById(R.id.btn_accueil);
-        imageButtonAccueil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreerPlanVoyageActivity.this, MainActivity.class);
-                startActivity(intent);
+        // Initialisation de BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Ajoutez un écouteur de navigation pour gérer les sélections de menu
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            // Utilisez une déclaration if-else if-else pour gérer les sélections de menu
+            if (itemId == R.id.navigation_home) {
+                // Appellez votre méthode pour charger le fragment de carte
+                loadMapFragment();
+                return true;
+            } else if (itemId == R.id.navigation_travel) {
+                // Gérez l'élément Voyages (par exemple, remplacer par un fragment de voyages)
+                // loadVoyagesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_add) {
+                // Redirige le bouton "Ajouter" vers l'activité CreerVoyageActivity
+                return true;
+            } else if (itemId == R.id.navigation_favorites) {
+                // Gérez l'élément Enregistrements (par exemple, remplacer par un fragment de favoris)
+                // loadFavoritesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_options) {
+                // Gérez l'élément Options (par exemple, remplacer par un fragment d'options)
+                // loadOptionsFragment();
+                return true;
             }
+
+            // Si aucun des éléments ne correspond, renvoyez false
+            return false;
         });
 
-        ImageButton imageButtonCreerVoyage = findViewById(R.id.btn_plus);
-        imageButtonCreerVoyage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreerPlanVoyageActivity.this, CreerVoyageActivity.class);
-                startActivity(intent);
-            }
-        });
 
+    }
+
+    private void loadMapFragment() {
+        MapFragment mapFragment = new MapFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container, mapFragment);
+
+        transaction.commit();
     }
 
 
--- app/src/main/java/uqac/dim/travelmanager/CreerVoyageActivity.java
+++ app/src/main/java/uqac/dim/travelmanager/CreerVoyageActivity.java
@@ -23,6 +23,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -139,23 +142,49 @@ public class CreerVoyageActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton imageButtonAccueil = findViewById(R.id.btn_accueil);
-        imageButtonAccueil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreerVoyageActivity.this, MainActivity.class);
-                startActivity(intent);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Ajoutez un écouteur de navigation pour gérer les sélections de menu
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            // Utilisez une déclaration if-else if-else pour gérer les sélections de menu
+            if (itemId == R.id.navigation_home) {
+                // Appellez votre méthode pour charger le fragment de carte
+                loadMapFragment();
+                return true;
+            } else if (itemId == R.id.navigation_travel) {
+                // Gérez l'élément Voyages (par exemple, remplacer par un fragment de voyages)
+                // loadVoyagesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_add) {
+                // Redirige le bouton "Ajouter" vers l'activité CreerVoyageActivity
+                return true;
+            } else if (itemId == R.id.navigation_favorites) {
+                // Gérez l'élément Enregistrements (par exemple, remplacer par un fragment de favoris)
+                // loadFavoritesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_options) {
+                // Gérez l'élément Options (par exemple, remplacer par un fragment d'options)
+                // loadOptionsFragment();
+                return true;
             }
+
+            // Si aucun des éléments ne correspond, renvoyez false
+            return false;
         });
 
-        ImageButton imageButtonCreerVoyage = findViewById(R.id.btn_plus);
-        imageButtonCreerVoyage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreerVoyageActivity.this, CreerVoyageActivity.class);
-                startActivity(intent);
-            }
-        });
+
+    }
+
+    private void loadMapFragment() {
+        MapFragment mapFragment = new MapFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container, mapFragment);
+
+        transaction.commit();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
