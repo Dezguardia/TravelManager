344946856f6bb4577e4c4025cd71b8aa6c7d1d31
hector <hx.euwx@gmail.com>
2024-04-27 17:52:16 -0400

    Nouvelle bottomnavbar sur activity_main.xml

--- app/src/main/java/uqac/dim/travelmanager/MainActivity.java
+++ app/src/main/java/uqac/dim/travelmanager/MainActivity.java
@@ -2,41 +2,78 @@ package uqac.dim.travelmanager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import uqac.dim.travelmanager.R;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    MenuInflater menuInflater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialisation de BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Ajoutez un écouteur de navigation pour gérer les sélections de menu
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
 
-        if (fragmentManager.findFragmentById(R.id.fragment_container) == null) {
-
-            loadMapFragment();
-        }
-
-        ImageButton imageButtonCreerVoyage = findViewById(R.id.btn_plus);
-        imageButtonCreerVoyage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            // Utilisez une déclaration if-else if-else pour gérer les sélections de menu
+            if (itemId == R.id.navigation_home) {
+                // Appellez votre méthode pour charger le fragment de carte
+                loadMapFragment();
+                return true;
+            } else if (itemId == R.id.navigation_travel) {
+                // Gérez l'élément Voyages (par exemple, remplacer par un fragment de voyages)
+                // loadVoyagesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_add) {
+                // Redirige le bouton "Ajouter" vers l'activité CreerVoyageActivity
                 Intent intent = new Intent(MainActivity.this, CreerVoyageActivity.class);
                 startActivity(intent);
+                return true;
+            } else if (itemId == R.id.navigation_favorites) {
+                // Gérez l'élément Enregistrements (par exemple, remplacer par un fragment de favoris)
+                // loadFavoritesFragment();
+                return true;
+            } else if (itemId == R.id.navigation_options) {
+                // Gérez l'élément Options (par exemple, remplacer par un fragment d'options)
+                // loadOptionsFragment();
+                return true;
             }
+
+            // Si aucun des éléments ne correspond, renvoyez false
+            return false;
         });
+
+        // Chargez le fragment de carte au démarrage s'il n'y en a pas déjà un
+        if (getSupportFragmentManager().findFragmentById(R.id.fragment_container) == null) {
+            loadMapFragment();
+        }
     }
 
+
+        // Vous avez déjà une écoute d'événement pour le bouton "Ajouter"
+        // Vous pouvez le laisser tel quel ou supprimer le code ci-dessous
+        // si vous utilisez `setOnItemSelectedListener` pour gérer le bouton "Ajouter"
+        // dans le menu de navigation.
+
+        // ImageButton imageButtonCreerVoyage = findViewById(R.id.btn_plus);
+        // imageButtonCreerVoyage.setOnClickListener(v -> {
+        //     Intent intent = new Intent(MainActivity.this, CreerVoyageActivity.class);
+        //     startActivity(intent);
+        // });
+
+
+    // Méthode pour charger le fragment de carte
     private void loadMapFragment() {
         MapFragment mapFragment = new MapFragment();
 
@@ -46,4 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
         transaction.commit();
     }
+
+    // Ajoutez d'autres méthodes comme loadVoyagesFragment(), loadFavoritesFragment(), loadOptionsFragment()
+    // pour gérer le remplacement des fragments en fonction des éléments sélectionnés dans BottomNavigationView
 }
